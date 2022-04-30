4d8cc875c942d378d1de1f692eb5f9ec4317ec56
jmsmf28 <jmsmf2011@gmail.com>
2022-04-30 09:36:35 +0100

    added motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,65 +4,74 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 
 
+const dropIn = {
+  hidden: { y: "-100vh", opacity: 0 },
+  visible: { y: 0, opacity: 1, transition: { duration: 0.5, type: "spring", stiffness: 500, damping: 100 } },
+  exit: { y: "-100vh", opacity: 0 }
+}
+
+const slideIn = {
+  hidden: { y: "-100vh", opacity: 0 },
+  visible: { y: 0, opacity: 1, transition: { duration: 0.5, type: "spring", stiffness: 500, damping: 100 } },
+  exit: { y: "-100vh", opacity: 0 }
+}
+
+
 const Home: NextPage = () => {
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2  bg-[url('/images/bg1.jpg')]">
       <Head>
         <title>Venda o seu imóvel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     
-      <main className="flex w-full flex-1 flex-col items-right justify-center px-20 text-center">
-        
+
+      <header className="text-center">
         <h1 className="text-6xl font-bold">
           Mude a sua vida !!!
         </h1>
+      </header>
 
+      <main className="flex w-full flex-col items-right justify-center px-20 text-center">
 
-        <div className="mt-6 flex max-w-4xl flex-col items-right justify-end sm:w-full">
-          {/*<a
-            href=""
-            className="mt-6 w-96 rounded-xl p-6 text-left"
-          >
-            <h3 className="text-2xl font-bold"></h3>
-            <p className="mt-4 text-xl">
-              
-            </p>
-  </a>*/}
+        <div>
 
-          <a
-            href=""
-            className="mt-6 w-96 rounded-xl border p-6 text-left border-gray-600 hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold text-white">Precisa de dinheiro ?</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          {/*<a
-            href=""
-            className="mt-6 w-96 rounded-xl p-6 text-left "
-          >
-            <h3 className="text-2xl font-bold"></h3>
-            <p className="mt-4 text-xl">
-         
-            </p>
-</a>*/}
-
-          <a
-            href=""
-            className="mt-6 w-96 rounded-xl border border-gray-600 p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Quer vender o seu imóvel ?</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <motion.div
+            className="mt-6 flex max-w-4xl flex-col items-right justify-end sm:w-full"
+            variants={dropIn}
+            initial="hidden"
+            animate="visible"
+            exit="exit">
+            <a
+              href=""
+              className="mt-6 w-96 rounded-xl border-2 p-6 text-left border-gray-600 hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Precisa de dinheiro ?</h3>
+              <p className="mt-4 text-xl">
+                Learn about Next.js in an interactive course with quizzes!
+              </p>
+            </a>
+            <a
+              href=""
+              className="mt-6 w-96 rounded-xl border-2 border-gray-600 p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Quer vender o seu imóvel ?</h3>
+              <p className="mt-4 text-xl">
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </motion.div>
+        </div>
+        <div>
+          <motion.div>
+            <button>
+              Contact
+            </button>
+          </motion.div>
         </div>
       </main>
 
-      <footer className="flex h-6 w-full items-center justify-center ">
+      <footer className="mt-12 flex h-6 w-full items-center justify-center ">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -73,7 +82,7 @@ const Home: NextPage = () => {
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={8} />
         </a>
       </footer>
-      </div>
+    </div>
 
   )
 }
